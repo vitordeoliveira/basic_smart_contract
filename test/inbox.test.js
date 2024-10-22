ee7e7a80a6085d46a9309efd27543ae134604b77
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-10-22 10:49:07 +0100

    basic test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = "INITIAL_MESSAGE";
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -17,11 +18,18 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hi there"],
+      arguments: [INITIAL_MESSAGE],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
-it("", () => {
-  console.log(inbox);
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
 });
