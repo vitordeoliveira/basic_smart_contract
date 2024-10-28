f7eafb7ab1bb0ee92133731bee76e480018272f4
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-10-28 08:44:02 +0000

    setMessage test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -32,4 +32,13 @@ describe("Inbox", () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, INITIAL_MESSAGE);
   });
+
+  it("can change the message", async () => {
+    const newMessage = "new message";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, newMessage);
+  });
 });
